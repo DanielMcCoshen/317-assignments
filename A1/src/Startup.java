5965275b72aee4ade9bce767c7abc98e14a26858
Daniel McCoshen <upstagecoronet@gmail.com>
2018-02-07 10:29:55 -0600

    isgoal

--- A1/src/Startup.java
+++ A1/src/Startup.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.LinkedList;
 
 /**
@@ -13,9 +14,8 @@ public class Startup {
     public static void main(char args[]) {
         State current = new State(1, 1, 1, 1);
 
-        while (current != null) {
+        while (!isGoal(current)) {
             LinkedList<State> successors =  successor(current);
-
             current = select(successors);
 
         }
@@ -28,13 +28,40 @@ public class Startup {
             float oldDist = t.getTruckDist(); //backup truck distance
 
             if (!t.isfull()){ //pick up packages
-
+                for (Package p : new LinkedList<Package>(prev.getAwaitingPickup())){
+                    //pick up package
+                    t.moveLocation(p.getSource());
+                    t.getPackages().add(p);
+                    prev.getAwaitingPickup().remove(p);
+                    prev.getEnRoute().add(p);
+                    //clone state
+                    toRet.add(new State(prev));
+                    //restore previous values
+                    prev.getEnRoute().remove(p);
+                    prev.getAwaitingPickup().add(p);
+                    t.getPackages().remove(p);
+                    System.arraycopy(oldLoc,  0, t.getLocation(), 0, t.getLocation().length);
+                    t.setTruckDist(oldDist);
+                }
             }
 
             if (t.getPackages().size() > 0){ //drop off packages
-
+                for(Package p : t.getPackages()){
+                    //drop off packages
+                    t.moveLocation(p.getDestination());
+                    t.getPackages().remove(p);
+                    prev.getEnRoute().remove(p);
+                    //clone state
+                    toRet.add(new State(prev));
+                    //restore previous values
+                    prev.getEnRoute().add(p);
+                    t.getPackages().add(p);
+                    System.arraycopy(oldLoc,  0, t.getLocation(), 0, t.getLocation().length);
+                    t.setTruckDist(oldDist);
+                }
             }
-
+            t.moveLocation(new float[oldLoc.length]); // move to the garage;
+            toRet.add(new State(prev));
         }
         return toRet;
     }
@@ -47,11 +74,24 @@ public class Startup {
                 current = eval;
             }
         }
+
         return current;
     }
 
     private static float heuristic(State state){ //TODO: estimates cost to goal
         return 0;
+    }
+
+    private static boolean isGoal(State eval){ //TODO: determine if at goal
+        boolean atGarage = true;
+        for (Truck t : eval.getTrucks()){
+            float[] garage = new float[t.getLocation().length];
+            if (!Arrays.equals(garage, t.getLocation())){
+                atGarage = false;
+            }
+        }
+
+        return atGarage && eval.getAwaitingPickup().size() == 0 && eval.getEnRoute().size() == 0;
     }
 
 }
